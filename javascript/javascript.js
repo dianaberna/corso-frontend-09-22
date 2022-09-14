ceebafa459f0c0557b0c047b018c6ed9ddba7444
Dario Purpi <dariopurpi@gmail.com>
2022-09-14 17:48:19 +0200

    primiEserciziJavascript

---
+++ javascript/javascript.js
@@ -0,0 +1,54 @@
+function moltiplicazione (a,b) {
+let somma= a*b
+return somma;
+
+}
+console.log(moltiplicazione(14,4))
+
+
+let a=-1
+
+
+if (a<=0){
+    console.log("true")}
+    else{
+    console.log("false")}
+
+
+
+    const x=365
+function giorni(b){
+let totale= b*x
+console.log(totale)}
+
+
+giorni(10)
+
+
+function zampe (polli,mucche,maiali){
+let totale=polli*2+mucche*4+maiali*4
+console.log(totale)}
+
+zampe(4,4,7)
+
+
+
+let z=87
+if (z==1){
+    console.log(7)}
+    else{
+    console.log(1+5*z)}
+
+
+    function ordinati(q)
+    
+
+
+
+    
+
+
+
+
+
+
