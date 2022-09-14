ef753bf3d1d5f83dde06c6f5144cffa0b1828997
zeeevq1 <zeeevq1@gmail.com>
2022-09-14 17:58:35 +0200

    primo giorno java

---
+++ style.js
@@ -0,0 +1,94 @@
+
+
+function moltiplicazione(a,b) {
+
+    let c = a*b;
+   console.log(c);
+}
+    console.log(moltiplicazione(3,2));
+    
+   
+
+
+
+    /*Crea una funzione che accetta un numero come unico argomento e 
+    restituisce true se è minore o uguale a zero, altrimenti restituisce false.*/
+
+let a=4
+let b=6
+
+if(a<=b){
+    console.log("true")}
+    else{
+        console.log("false")
+    }
+
+    /*Crea una funzione che prenda l'età in anni e restituisca l'età in giorni*/
+
+function calcolaanni(a,b) {
+    let c= a*b;
+    console.log(c);
+}
+    console.log(calcolaanni(25,365));
+
+    /*in questa sfida, un contadino ti chiede di dirgli quante zampe si possono contare tra tutti i suoi animali. L'agricoltore alleva tre specie: polli = 2 zampe, mucche = 4 zampe, maiali = 4 zampe
+L'agricoltore ha contato i suoi animali e ti dà un subtotale per ogni specie. Devi implementare una funzione che restituisca il numero totale di zampe di tutti gli animali. */
+
+
+
+function calcolozampe(polli,maiali,mucche){
+    let totale = polli*2+mucche*4+maiali*4;
+    console.log(totale);
+}
+    
+
+    console.log(calcolozampe(5,7,10));
+
+
+    /*Crea una funzione che accetta un numero come argomento. 
+    Somma tutti i numeri da 1 al numero passato alla funzione. 
+    Ad esempio, se l'input è 4, la tua funzione dovrebbe restituire 
+    10 perché 1 + 2 + 3 + 4 = 10.*/
+
+    function Somma(a,b,c,d){
+        let e = a+b+c+d;
+        console.log(e);
+    }
+    console.log(Somma(1,2,3,));
+
+    /*Crea una funzione che prenda un numero (step) 
+    come argomento e restituisca il numero di fiammiferi in 
+    quello step. Vedere i passaggi 1, 2 e 3 nell'immagine.*/
+    function ("ciaone") + ("ciao"){
+        if{}
+        console.log(b);
+
+    }
+    console.log(houses());
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
