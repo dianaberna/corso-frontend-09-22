d13e0024a485f6075653294f3fd88ddbeb296080
Teresa Godino <tamshopinfo@gmail.com>
2022-09-14 18:05:40 +0200

    javascript

---
+++ esercizi/nuovo.js
@@ -0,0 +1,70 @@
+
+console.log("ciao mondo");
+
+//variabili
+console.log("variabile a");
+var a;
+a = 1;
+console.log(a);
+//a = ciao;
+
+console.log("variabile b")
+let b = 2;
+console.log(b)
+b = 10;
+a = a + "ciao";
+console.log(typeof(a));
+
+//function
+function somma(a, b){
+    let risultato=a+b;
+    console.log(risultato);
+}
+
+somma(1, 6,);
+somma (5, 3,);
+somma (5, 5);
+
+
+
+/*function multiplication (a, b){
+    let risultato=a*b;
+  //  console.log(risultato);
+  return risultato
+}
+let r=multiplication (10, 3)
+console.log(r)
+
+
+//multiplication (5, 5)
+
+//console.log("costante")
+//const costante = 3;*/
+
+function disuguaglianza(a){
+if( a<=0 ){
+    console.log("vero")
+}
+else {
+    console.log("falso")}}
+
+ disuguaglianza(9)
+
+ function giorni(età){
+    let risultato=età*365;
+    console.log(risultato);
+    return risultato
+}
+ let età=65
+giorni(età)
+
+
+function zampe(polli, mucca, maiali)
+{ let zampepolli=polli*2;
+    let zampemucca=mucca*4;
+    let zampemaiali=maiali*4;
+    let risultato=zampepolli+zampemaiali+zampemucca
+    return risultato
+}
+   
+    console.log (zampe(2,3,5))
