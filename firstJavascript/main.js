98cdedb0ef1a03823d3f74a90c574cb9d5dccea5
Christopher <tchry902@gmail.com>
2022-09-15 15:55:44 +0200

    project js

--- firstJavascript/main.js
+++ firstJavascript/main.js
@@ -1,23 +1,23 @@
 // Crea una funzione che accetta due numeri come argomenti e ne restituisce la moltiplicazione.
 
-const moltiplicazione = (som1, som2) => console.log(som1 * som2);
+/* const moltiplicazione = (som1, som2) => console.log(som1 * som2);
 
-moltiplicazione(2, 5);
+moltiplicazione(2, 5); */
 
 // Crea una funzione che accetta un numero come unico argomento e restituisce true se è minore o uguale a zero, altrimenti restituisce false.
-const confronto = (num) => console.log(num <= 0);
+/* const confronto = (num) => console.log(num <= 0);
 
 confronto(4);
-
+ */
 // Creare una funzione che prenda l'età in anni e restituisca l'età in giorni.
-const calcGiorni = (age) => console.log("age", age * 365);
+/* const calcGiorni = (age) => console.log("age", age * 365);
 
-calcGiorni(65);
+calcGiorni(65); */
 
 // In questa sfida, un contadino ti chiede di dirgli quante zampe si possono contare tra tutti i suoi animali. L'agricoltore alleva tre specie: polli = 2 zampe, mucche = 4 zampe, maiali = 4 zampe
 // L'agricoltore ha contato i suoi animali e ti dà un subtotale per ogni specie. Devi implementare una funzione che restituisca il numero totale di zampe di tutti gli animali.
 
-const polli = 20;
+/* const polli = 20;
 
 const mucche = 14;
 
@@ -32,4 +32,16 @@ const somma = (animal) => {
     return console.log(`totale zampe mucche o maiali`, animal * 4);
   }
 };
-somma(maiali);
+somma(maiali); */
+
+const alfabeto = "abcdefghilmnopqrstuz".split("");
+
+const numberAlfa = (alfabeto, numero) => {
+  if (numero == String || numero > 20) {
+    console.log("inserire valore corretto numero o <20");
+  } else {
+    return console.log(alfabeto[numero - 1]);
+  }
+};
+console.log(alfabeto);
+numberAlfa(alfabeto, 1);
