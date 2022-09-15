2c565cddfcc91442f10c93ecec27d45858056bea
Valentina Bernacchia <vbernacchia02@gmail.com>
2022-09-15 18:32:37 +0200

    revisione, ma comunque problematico nel secondo if

--- Javascript/Email.js
+++ Javascript/Email.js
@@ -1,25 +1,20 @@
-function ControlloEmail(email){
-    if(email.includes("@") && email.includes(".") && (email.indexOf('@') != 0) )
-    {
-       //email.slice(0,3)
-
-         if(email.indexOf('@') <= email.indexOf('.')){
-
-            return true;
-            }
-    
-            else {
-                return false;
-            }
-                
-    }
-
- else {
-    return false;
+function ControlloEmail(email) {
+   if (email.includes("@") && email.includes(".") && email.indexOf("@") != 0 && email.indexOf(".") !=0)  {
+     let parte = email.split("@");
+     console.log(parte);
+ 
+     if (
+       parte[0].indexOf("@") >= parte[0].indexOf(".") ||
+       parte[1].indexOf("@") <= parte[1].indexOf(".")
+     ) {
+       return true;
+     } else {
+       return false;
+     }
+   } else {
+     return false;
+   }
  }
-    
-    
-}
-
-let email=prompt(" inserisci email");
- console.log(ControlloEmail(email)); 
+ 
+ let email = prompt(" inserisci email");
+ console.log(ControlloEmail(email));
