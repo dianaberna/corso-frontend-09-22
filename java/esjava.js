7feed69b34f5ea0150a202fcba7d399694326870
Alessandro Amodio <alessandroamodio03@gmail.com>
2022-09-16 08:59:04 +0200

    modifica Amod

---
+++ java/esjava.js
@@ -0,0 +1,83 @@
+function moltiplicazione (a,b) {
+
+    let risultato=a*b
+    console.log (risultato)
+
+}
+moltiplicazione (5,2)
+
+
+function diseguaglianza (a) {
+    if (a<=0)
+    {console.log("true")}
+    else
+    {console.log("false")}
+}
+diseguaglianza (0)
+
+function giorni (età) {
+    let risultato=età*365;
+    console.log = (risultato)
+    return risultato
+}
+let età=40
+giorni(età)
+
+function zampe(polli,mucche,maiali)
+{ let zampepolli=polli*2
+    let zampemucche=mucche*4
+    let zampemaiali=maiali*4
+    let risultato=zampepolli+zampemaiali+zampemucche
+    return risultato
+
+}
+console.log (zampe(2,3,5))
+
+function addUp(e) {
+    let f=0;
+    for(i=0;i<=e;i++) {
+        f=f+1;
+    }
+return f;
+}
+    let g=console.log(addUp(e));
+
+/*let h;
+function fiammiferi(h) {
+    h*6
+
+}   */
+
+function addUp(a) {
+    let numero=0
+    let somma=0
+    while (numer0 < a) {
+
+        somma= somma+numero;
+        numero ++;
+
+    }
+    console.log(somma);
+}
+
+function addUp(a) {
+    let numero=0
+    let somma=0
+    while (numero < a) {
+
+        somma= somma+numero;
+        numero ++;
+    }
+    console.log(somma);
+}
+
+console.log("il risultato della funzione addUp è: ");
+addUp(4);
+
+    //posizione alfabeto (switch case)
+/*
+let numero=0;
+/* let lettera=a; */
+
+
+
