030f0d866d23b2fdfaa7f5996aa85959aea83f2b
lorenzo <lorenzo.becchetti@libero.it>
2022-09-17 12:06:31 +0200

    dom

--- dom.js
+++ dom.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", function(){
 
     console.log("ciao dom" );
-document.getElementById("rosa") .style.backgroundColor= "red";
-
+/* document.getElementById("rosa") .style.backgroundColor= "red";
+ */
 
 
 /* let a= document.getElementsByTagName("p");
@@ -17,13 +17,35 @@ for(i=0;i<a.length; i++){
  */
 
 
-let risultato=document.getElementsByTagName ("p")
+/* let risultato=document.getElementsByTagName ("p")
 
 let indice=0
 while(indice<risultato.length){
 risultato[indice].style.backgroundColor= "orange";
-indice++
-}
+indice++ */
+
+/* } */
+
+
+let pulsante= document.createElement("button")
+let testoPulsante= document.createTextNode("perchè non cliccarlo")
+pulsante.id= "pulsante";
+pulsante.appendChild(testoPulsante);
+let rosa=document.getElementById("rosa")
+rosa.appendChild(pulsante);
+pulsante.addEventListener("click",function(){
+  alert("è stato installato un virus correttamente");
+});
+
+
+let paragrafo=document.createElement("p")
+let testoParagrafo= document.createTextNode("oggi non piove")
+paragrafo.id="meteo";
+paragrafo.appendChild(testoParagrafo);
+let primopar=document.getElementById("primopar")
+primopar.insertAdjacentElement("afterend",paragrafo);
+
+
+
+
 })
-
-
