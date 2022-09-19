afe50f796d86d8237050875622ad99382f3ae0fa
AlessandraMagri <screecedesign@hotmail.it>
2022-09-19 10:23:56 +0200

    modifica

---
+++ css/paginaweb.js
@@ -0,0 +1,11 @@
+window.addEventListener("load",function(){
+    console.log("ciaooo");
+    document.getElementsByClassName("background")[0].style.backgroundColor="#F6EBDA;";
+    document.getElementsByClassName("container");
+
+    let images = document.getElementById("image").src ="unsplash_3n3mPoGko8g.png";
+   
+
+    const titoli = document.createElement("H2, H3");
+    const p = document.createTextNode("p");
+})
