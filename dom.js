d532a2756841c590c8d75303f7f00aedc798d7f3
theflaviomercuri <theflaviomercuri@gmail.com>
2022-09-19 09:49:40 +0200

    aggiornamento a JavaScript per dashboard

---
+++ dom.js
@@ -0,0 +1,510 @@
+console.clear;
+
+/* 
+//FUNZIONI
+
+function addNumToStr(aNumber, aString)
+{
+    let strtoint = parseInt(aString); 
+    return aNumber+strtoint;
+}
+
+function star(a,b){return a*b;}
+
+function minOMax(aNumber){
+    return a <= 0 ? true : false
+     /* 
+    if(aNumber>=0){
+        return true;
+    }
+    else{
+        return false;
+    } */
+/*
+}
+
+function anniInGiorni(anni){
+    return anni*365;
+}
+
+function contaZampe(polli, mucche, maiali){
+    const zPolli=2, zMucche=4, zMaiali=4;
+    return (zPolli*polli)+(zMucche*mucche)+(zMaiali*maiali);
+}
+
+function superSomma(aNumber){
+    let somma=0;
+    for (i=aNumber; i>0; i--) {
+        somma+=i;
+    }
+    return somma;
+}
+
+function matchHouses(step){
+    //if(step==0){return 0;}else{return (step*6)-(step-1);}
+    return (step==0) ? 0 : (step*6)-(step-1)
+}
+ */
+
+
+//CORPO
+
+/* let num = addNumToStr(4, "4");
+let tipo = typeof(num);
+console.log(`num è:${num}, il tipo di num è:${tipo}`);
+
+let a=3, b=4;
+console.log(`Il prodotto a*b=${star(a,b)}`);
+
+console.log("E' minore uguale a zero? "+minOMax(prompt("Inserisci un numero")));
+console.log("I tuoi anni corrispondono a "+anniInGiorni(12)+" giorni");
+console.log("Ai tuoi animali corrispondono "+contaZampe(5,5,5)+" zampe");
+console.log("La tua super somma è "+superSomma(10));
+console.log("Il tuo match conta "+matchHouses(1)+" fiammiferi");
+ */
+
+
+/* 
+function getLettersFromNumbers(aNumber){
+    
+    let str1="invalid";
+    let stringa="abcdefghilmnopqrstuvz";
+    aNumber--;
+
+    if(aNumber>=0 && aNumber<=20)
+    {
+        let cod=stringa.charCodeAt(aNumber);
+        str1=String.fromCharCode(cod);
+    }
+    
+    console.log(`Al numero ${++aNumber} corrisponde la lettera:${str1}`)
+    return str1;
+
+}
+
+getLettersFromNumbers(prompt("Inserisci un numero da 1 a 21"));
+
+ */
+
+
+/* function scontato(prezzo, sconto){
+    
+    let prezzofinale = (prezzo-((prezzo/100) * sconto));
+    console.log(prezzofinale);
+    return prezzofinale.toFixed(2);
+}
+
+
+console.log("Il prezzo dopo lo sconto è "+scontato(prompt("Inserisci il prezzo pieno:"),prompt("Inserisci la percentuale di sconto:"))+"€"); 
+*/
+
+/* 
+function yourOp(fat1, op, fat2){
+    
+    let res=0;
+
+    switch(op){
+        case "+":
+            res=fat1+fat2;
+            break;
+        case "-":
+            res=fat1-fat2;
+            break;
+        case "*":
+            res=fat1*fat2;
+            break;
+        case "x":
+            res=fat1*fat2;
+            break;
+        case "X":
+            res=fat1*fat2;
+            break;
+        case ":":
+            if(fat2!==0){
+                res=fat1/fat2;
+            }else{res="impossibile dividere per 0!!"}
+            break;
+        case "/":
+            if(fat2!==0){
+                res=fat1/fat2;
+            }else{res="impossibile dividere per 0!!"}
+            break;
+        default:
+            res="Operatore non riconosciuto."
+    }
+    return res;
+}
+
+console.log("Il risultato è "+yourOp(prompt("Inserisci il primo fattore:"),prompt("Inserisci l'operatore:"),prompt("Inserisci il secondo fattore:")));
+*/
+
+
+/* function sommaArray(anArray){
+    let sum=0;
+    for(let i=0; i<anArray.length; i++){
+        sum+=anArray[i];
+    }
+    return sum;
+}
+
+let myArray = [2,3,4,10];
+console.log(`La somma dell'array è ${sommaArray(myArray)}`);
+ */
+
+/* 
+function quantiTrue(anArray){
+    let sum=0;
+    for(let i=0; i<anArray.length; i++){
+        if(anArray[i]==true){sum++};
+    }
+    return "Trovi nell'array "+sum+" TRUE su "+anArray.length+" elementi.";
+}
+
+let myArray = [true,false,false,true,false,true];
+console.log(quantiTrue(myArray));
+
+ */
+
+/* 
+    let exp=prompt("Quante volte piego (da 1 in poi)?");
+    let res= ((0.5*(Math.pow(2,exp)-1))+0.5)/1000;
+    console.log("Dopo la piega la carta sarà spessa: "+res+"m");
+     */
+
+/* 
+function validateEmail()
+{
+    let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+    let testEmails = prompt("Mail:");//["notanemail.com", "workingexample@email.com", "another_working@somethingelse.org", "notworking@1.com"];
+    console.log(regex.test(testEmails));
+}
+
+validateEmail();
+
+ */
+
+
+/* ++++++++++++SOLUZIONI++++++++++++ */
+
+/*
+// moltiplicazione di due numeri
+console.log("moltiplicazione di due numeri");
+function moltiplicazione(a, b) {
+    return a * b;
+}
+
+console.log(moltiplicazione(3, 2));
+console.log(moltiplicazione(-3, -6));
+console.log(moltiplicazione(7, 3));
+
+// convertire anni in giorni
+console.log("convertire anni in giorni");
+function anniInGiorni(anni) {
+    return anni * 365;
+}
+console.log(anniInGiorni(65));
+console.log(anniInGiorni(0));
+console.log(anniInGiorni(20));
+
+// minore o uguale a zero
+console.log("minore o uguale a zero");
+function minoreUgaleAZero(a) {
+    if (a <= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(minoreUgaleAZero(5));
+console.log(minoreUgaleAZero(0));
+console.log(minoreUgaleAZero(-2));
+
+function minoreUgaleAZero(a) {
+    return a <= 0 ? true : false;
+}
+console.log(minoreUgaleAZero(5));
+console.log(minoreUgaleAZero(0));
+console.log(minoreUgaleAZero(-2));
+
+// problema della fattoria
+console.log("problema della fattoria");
+function numeroZampe(polli, mucche, maiali) {
+    return polli * 2 + (mucche + maiali) * 4;
+}
+console.log(numeroZampe(2, 3, 5));
+console.log(numeroZampe(1, 2, 3));
+console.log(numeroZampe(5, 2, 8));
+
+// casa di fiammiferi
+// 1 casa = 6 fiammiferi (1*5 + 1 = 6)
+// 2 case = 11 fiammiferi (2*5 + 1 = 11)
+// 3 case = 16 fiammiferi (3*5 + 1 = 16)
+
+console.log("casa di fiammiferi");
+function numeroDiFiammiferi(step) {
+    return step == 0 ? 0 : step * 5 + 1;
+}
+console.log(numeroDiFiammiferi(0)); // se step=0 restituisce 0
+console.log(numeroDiFiammiferi(1));
+console.log(numeroDiFiammiferi(4));
+console.log(numeroDiFiammiferi(87));
+
+// somma i numeri da un unico numero
+console.log("somma i numeri da un unico numero");
+function sommaDaUnUnicoNumero(numero) {
+    let somma = 0;
+    for (let i = numero; i > 0; i--) {
+        somma += i;
+    }
+    return somma;
+}
+console.log(sommaDaUnUnicoNumero(4));
+console.log(sommaDaUnUnicoNumero(13));
+console.log(sommaDaUnUnicoNumero(600));
+
+// posizione nell'alfabeto
+console.log("posizione nell'alfabeto ");
+function letteraInPosizione(posizione) {
+    if (posizione >= 1 && posizione <= 26 && posizione % 1 == 0) {
+        return String.fromCharCode(posizione + 96);
+    } else {
+        return "invalid";
+    }
+}
+
+console.log(letteraInPosizione(1));
+console.log(letteraInPosizione(26.0));
+console.log(letteraInPosizione(0));
+console.log(letteraInPosizione(4.5));
+
+// trova lo sconto
+console.log("trova lo sconto");
+function trovaSconto(prezzo, sconto) {
+    return prezzo - (prezzo * sconto) / 100;
+}
+
+console.log(trovaSconto(1500, 50));
+console.log(trovaSconto(89, 20));
+console.log(trovaSconto(100, 75));
+
+// calcolatrice di base
+console.log("calcolatrice di base");
+function calcolatrice(a, operazione, b) {
+    switch (operazione) {
+        case "+":
+            return a + b;
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "/": {
+            return b == 0 ? "Impossibile dividere per 0!" : a / b;
+        }
+    }
+}
+
+console.log(calcolatrice(2, "/", 0));
+console.log(calcolatrice(2, "+", 2));
+console.log(calcolatrice(2, "*", 2));
+console.log(calcolatrice(4, "/", 2));
+
+// somma di n numeri
+console.log("somma di elementi array ");
+function sommaElementiArray(array) {
+    let somma = 0;
+    for (let i = 0; i < array.length; i++) {
+        somma = somma + array[i];
+    }
+    return somma;
+}
+
+console.log(sommaElementiArray([1, 2, 4]));
+console.log(sommaElementiArray([4, 2, 10, 2]));
+console.log(sommaElementiArray([1]));
+
+// quante volte è vero
+console.log("quante volte è vero");
+function quantiTrue(array) {
+    let contatore = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == true) {
+            contatore++;
+        }
+    }
+    return contatore;
+}
+console.log(quantiTrue([true, false, false, true, false]));
+console.log(quantiTrue([false, false, false, false]));
+console.log(quantiTrue([]));
+
+// piegare un pezzo di carta
+console.log("piegare un pezzo di carta");
+function spessore(numero) {
+    return 0.0005 * 2 ** numero;
+}
+console.log(spessore(1));
+console.log(spessore(4));
+console.log(spessore(21));
+
+// validare un'e-mail
+console.log("validare un'e-mail");
+function validareEmail(email) {
+    if (
+        email.indexOf("@") < 1 ||
+        email.indexOf(".") < 0 ||
+        email.indexOf("@") > email.lastIndexOf(".")
+    )
+        return false;
+    return true;
+}
+
+function validareEmail2(email) {
+    return /.+@.+\..+/.test(email);
+}
+
+console.log(validareEmail("@gmail.com"));
+console.log(validareEmail("hello.gmail@com"));
+console.log(validareEmail("gmail"));
+console.log(validareEmail("hello@gmail"));
+console.log(validareEmail("hello@edabit.com"));
+
+// sasso carta forbici
+console.log("sasso carta forbici");
+function sassoCartaForbici(p1, p2) {
+    return p1 == p2 ? `It's a draw` 
+        : p1 == `Rock` && p2 == "Scissors" ? `The winner is p1` : p1 == `Rock` ? `The winner is p2` 
+        : p1 == `Paper` && p2 == `Scissors` ? `The winner is p2` 
+        : p1 == `Paper` && p2 == `Rock`
+        ? `The winner is p1`
+        : p1 == `Scissors` && p2 == `Paper`
+        ? `The winner is p1`
+        : `The winner is p2`;
+}
+
+function sassoCartaForbici2(p1, p2) {
+    let w = {
+        Rock: "Scissors",
+        Scissors: "Paper",
+        Paper: "Rock",
+    };
+    if (p1 === p2) return "It's a draw";
+    return `The winner is ${w[p1] === p2 ? "p1" : "p2"}`;
+}
+
+console.log(sassoCartaForbici("Rock", "Paper"))
+console.log(sassoCartaForbici("Scissors", "Paper"))
+console.log(sassoCartaForbici("Paper", "Paper"))
+
+
+// quanti giorni tra due date
+console.log("quanti giorni tra due date")
+function quantiGiorni(primaData, secondaData){
+    const unGiorno = 24 * 60 * 60 * 1000;
+    console.log("unGiorno: "+unGiorno+" prima data: "+primaData+" seconda data:"+secondaData);
+    return diffDays = Math.round(Math.abs((primaData - secondaData) / unGiorno));
+}
+
+console.log(quantiGiorni(new Date("June 14, 2019"), new Date("June 20, 2019")))
+console.log(quantiGiorni(new Date("December 29, 2018"), new Date("January 1, 2019")))
+console.log(quantiGiorni(new Date("July 20, 2019"), new Date("July 30, 2019")))
+
+
+// rimuovere i duplicati di un array
+console.log("rimuovere i duplicati di un array");
+function soloUnici(valore, indice, singolo) {
+    return singolo.indexOf(valore) === indice;
+}
+
+function rimuoviDuplicati(array) {
+    let risultato = array.filter(soloUnici);
+    return risultato
+}
+console.log(rimuoviDuplicati([1, 0, 1, 0]));
+console.log(rimuoviDuplicati(["The", "big", "cat"]));
+console.log(rimuoviDuplicati(["John", "Taylor", "John"]));
+
+
+
+// budget totale
+console.log("budget totale");
+function budgetTotale(oggetto) {
+    let totale = 0;
+    console.log(oggetto.length);
+    for (let i = 0; i < oggetto.length; i++) {
+        totale = totale + oggetto[i].budget;
+    }
+
+    return totale;
+}
+
+console.log(
+    budgetTotale([
+        { name: "John", age: 21, budget: 23000 },
+        { name: "Steve", age: 32, budget: 40000 },
+        { name: "Martin", age: 16, budget: 2700 },
+    ])
+);
+
+console.log(
+    budgetTotale([
+        { name: "John", age: 21, budget: 29000 },
+        { name: "Steve", age: 32, budget: 32000 },
+        { name: "Martin", age: 16, budget: 1600 },
+    ])
+);
+*/
+/*
+// prezzo totale alimenti
+console.log("prezzo totale alimenti");
+function prezzoTotaleAlimenti(alimenti) {
+    let totale = 0;
+    for (let i = 0; i < alimenti.length; i++) {
+        totale = totale + (alimenti[i].price * alimenti[i].quantity);
+    }
+
+    return totale.toFixed(2);
+}
+console.log(
+    prezzoTotaleAlimenti([{ product: "milk", quantity: 1, price: 1.5 }])
+);
+console.log(
+    prezzoTotaleAlimenti([
+        { product: "milk", quantity: 1, price: 1.5 },
+        { product: "cereals", quantity: 1, price: 2.5 },
+    ])
+);
+console.log(
+    prezzoTotaleAlimenti([{ product: "milk", quantity: 3, price: 1.5 }])
+);
+console.log(
+    prezzoTotaleAlimenti([
+        { product: "milk", quantity: 1, price: 1.5 },
+        { product: "eggs", quantity: 12, price: 0.10 },
+        { product: "bread", quantity: 2, price: 1.6 },
+        { product: "cheese", quantity: 1, price: 4.5 },
+    ])
+);
+console.log(
+    prezzoTotaleAlimenti([
+        { product: "chocolate", quantity: 1, price: 0.10 },
+        { product: "lollipop", quantity: 1, price: 0.20 },
+    ])
+);
+*/
+
+
+function preventivo(listaSpesa){
+    let tot = 0;
+    console.log(listaSpesa.lenght);
+    for(let i=0; i<listaSpesa.lenght; i++)
+    {
+        tot= tot+listaSpesa[i].prezzo;
+    }
+    return tot;
+}
+
+console.log(preventivo([
+    { prodotto: "pane", qta: 1, prezzo: 4 },
+    { prodotto: "cereali", qta: 5, prezzo: 2.7 },
+    { prodotto: "vino", qta: 2, prezzo: 6 },
+]));
--- motiv assets/javascript/script.js
+++ motiv assets/javascript/script.js
@@ -1,61 +1,60 @@
 console.clear;
 
-//FUNZIONI
+window.addEventListener("load", function(){
 
-function addNumToStr(aNumber, aString)
-{
-    let strtoint = parseInt(aString); 
-    return aNumber+strtoint;
-}
-
-function star(a,b){return a*b;}
-
-function minOMax(aNumber){
-    return a <= 0 ? true : false
-    /* 
-    if(aNumber>=0){
-        return true;
+    function ramificaFratelli(parentTag, tag, attributo, valoriAtt){
+        
+        for(let i=0; i<tag.length; i++){
+            let unTag = document.createElement(tag[i]);
+            if(attributo[i]!==null){
+                unTag.setAttribute(attributo[i],valoriAtt[i]);
+            }
+            parentTag.appendChild(unTag);    
+        }
+        return true; //in caso.. di controllo su buon fine o aggiunta gestione errore
     }
-    else{
-        return false;
-    } */
-}
 
-function anniInGiorni(anni){
-    return anni*365;
-}
 
-function contaZampe(polli, mucche, maiali){
-    const zPolli=2, zMucche=4, zMaiali=4;
-    return (zPolli*polli)+(zMucche*mucche)+(zMaiali*maiali);
-}
+    //creo <main></main> in body
+    let main = document.createElement("main");
+    document.body.appendChild(main);
 
-function superSomma(aNumber){
-    let somma=0;
-    for (i=aNumber; i>0; i--) {
-        somma+=i;
+    //creo primo annidamento
+    ramificaFratelli(main,["div","div"],["class","class"],["barra_sx","content"]);
+
+    //creo secondo livello
+    let barra_sx=document.querySelector("main .barra_sx");
+    ramificaFratelli(barra_sx,["div","div"],["class","class"],["logo","voci_sx"]);
+    let content=document.querySelector("main .content");
+    ramificaFratelli(content,["div","div"],["class","class"],["nav","belly"]);
+
+    //creo terzo livello
+    let logo=document.querySelector("main .barra_sx .logo");
+    ramificaFratelli(logo,["span","span"],[null,null]);
+    document.querySelector("main .barra_sx .logo span + span").innerHTML="Motiv.";
+    
+    //struttura pulsantiera a sinistra nel terzo livello
+    let nBottoni = 8; //si lascia modificabile il numero di bottoni
+    let pulsantiera=document.querySelector("main .barra_sx .voci_sx");
+    for(let i=0; i<nBottoni; i++){
+        ramificaFratelli(pulsantiera,["button"],["class"],["menu_buttons"]);
     }
-    return somma;
-}
-
-function matchHouses(step){
-    //if(step==0){return 0;}else{return (step*6)-(step-1);}
-    return (step==0) ? 0 : (step*6)-(step-1)
-}
 
 
+    //creo quarto livello
+    let SVGlogoCode = `<svg width="27" height="27" viewBox="0 0 27 27" fill="none" xmlns="http://www.w3.org/2000/svg"> <rect y="0.0742188" width="26.914" height="26.85" rx="6" fill="#A162F7"/> <path d="M9.1273 19.7063L10.8492 17.9843C6.48698 15.344 7.21403 11.3357 8.12284 9.6616L13.5039 14.8992L18.885 9.6616C20.9513 14.1387 17.8805 17.0277 16.0868 17.9126L17.9523 19.7063C24.6248 14.6839 21.3961 7.93966 18.885 6.07422L13.5039 11.4553L7.97934 6.07422C2.09602 11.814 6.25741 18.2713 9.1273 19.7063Z" fill="white"/> </svg>`;
+    document.querySelector("main .barra_sx .logo > span").innerHTML = SVGlogoCode;
+    
+    
+    //(4° lvl bottoni) creo i percorsi per gli SVG delle icone per il gruppo bottoni, gli span dentro ai bottoni e li completo
+    let = svg_common_path = `<svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">`;
 
-//CORPO
-
-let num = addNumToStr(4, "4");
-let tipo = typeof(num);
-console.log(`num è:${num}, il tipo di num è:${tipo}`);
-
-let a=3, b=4;
-console.log(`Il prodotto a*b=${star(a,b)}`);
-
-console.log("E' minore uguale a zero? "+minOMax(prompt("Inserisci un numero")));
-console.log("I tuoi anni corrispondono a "+anniInGiorni(12)+" giorni");
-console.log("Ai tuoi animali corrispondono "+contaZampe(5,5,5)+" zampe");
-console.log("La tua super somma è "+superSomma(10));
-console.log("Il tuo match conta "+matchHouses(1)+" fiammiferi");
+    let bottoni_sx=document.querySelectorAll("main .barra_sx .voci_sx .menu_buttons");
+    for(let i=0; i<nBottoni; i++){
+        ramificaFratelli(bottoni_sx[i],["span","span"],[null,null]);
+        let span_icona=bottoni_sx[i].querySelectorAll("span")[0];
+        let span_testo=bottoni_sx[i].querySelectorAll("span")[1];
+        span_icona.innerHTML="icona";
+        span_testo.innerHTML="testo";
+    }
+});
