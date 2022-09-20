ded6e4b8b26010ca56963c2c1fca263aa0146bff
Giovanni Brienza <gio.brienza@gmail.com>
2022-09-20 13:25:00 +0200

    173 Javascript v.1.0

--- 173/173.js
+++ 173/173.js
@@ -1,3 +1,6 @@
+
+import data from './data.json'  assert {type: 'json'};
+
 window.addEventListener("load", ()=> {
         buildHeader();
         buildFilters();
@@ -98,11 +101,74 @@ window.addEventListener("load", ()=> {
     let resultsContainer = document.createElement("div");
     resultsContainer.setAttribute("class", "results-container");
     mainNode.appendChild(resultsContainer);
+    resultsContainer.appendChild(createHotelCard("Hotel Green", "./Assets/1246280_16061017110043391702.jpg", "&#xe838&#xe838&#xe838&#xe838&#xe838", "200", "450$"));
+
+    data.forEach(element => {
+        var starConverter = "";
+        for (let i=0; i<element.HotelStars; i++)
+        {
+            starConverter = starConverter+"&#xe838";
+        }
+        resultsContainer.appendChild(createHotelCard(element.HotelName, element.HotelImage, starConverter, element.HotelReviewCount, element.HotelPrice));
+    });
+ }
+
+ function createHotelCard(hotelName, hotelImage, hotelRating, hotelReviewCount, hotelPrice)
+ {
     let divHotelCard = document.createElement("div");
     divHotelCard.setAttribute("class", "hotel-card");
-    resultsContainer.appendChild(divHotelCard);
     let favButton = document.createElement("button");
     favButton.setAttribute("class", "button");
+    divHotelCard.appendChild(favButton);
     let favIcon = document.createElement("i");
+    favIcon.setAttribute("class", "material-symbols-outlined");
     favIcon.innerHTML = "favorite";
+    favButton.appendChild(favIcon);
+    let imgHotel = document.createElement("img");
+    imgHotel.setAttribute("alt", "Hotel Image");
+    divHotelCard.appendChild(imgHotel);
+    let divHotelInfo = document.createElement("div");
+    divHotelInfo.setAttribute("class", "hotel-info");
+    divHotelCard.appendChild(divHotelInfo);
+    let hotelNameH2 = document.createElement("h2");
+    divHotelInfo.appendChild(hotelNameH2);
+    let divRatingContainer = document.createElement("div");
+    divRatingContainer.setAttribute("class", "rating-container");
+    divHotelInfo.appendChild(divRatingContainer);
+    let hotelStars = document.createElement("i");
+    hotelStars.setAttribute("class", "material-symbols-rounded");
+    divRatingContainer.appendChild(hotelStars);
+    let reviewsCount = document.createElement("span");
+    divRatingContainer.appendChild(reviewsCount);
+    let divHotelPriceInfo = document.createElement("div");
+    divHotelPriceInfo.setAttribute("class", "hotel-price-info");
+    divHotelInfo.appendChild(divHotelPriceInfo);
+    let firstDivAfterHotelPriceInfo = document.createElement("div");
+    divHotelPriceInfo.appendChild(firstDivAfterHotelPriceInfo);
+    let hotelPriceH3 = document.createElement("h3");
+    firstDivAfterHotelPriceInfo.appendChild(hotelPriceH3);
+    let firstDivAfterFirstDiv = document.createElement("div");
+    firstDivAfterHotelPriceInfo.appendChild(firstDivAfterFirstDiv);
+    let bookingSpan = document.createElement("span");
+    bookingSpan.innerHTML = "Booking.com";
+    firstDivAfterFirstDiv.appendChild(bookingSpan);
+    let arrowIcon = document.createElement("i");
+    arrowIcon.setAttribute("class", "material-symbols-rounded");
+    arrowIcon.innerHTML = "&#xf8ce";
+    firstDivAfterFirstDiv.appendChild(arrowIcon);
+    let viewDealButton = document.createElement("button");
+    viewDealButton.setAttribute("type", "button");
+    viewDealButton.setAttribute("class", "button-style");
+    viewDealButton.innerHTML = "View deal";
+    divHotelPriceInfo.appendChild(viewDealButton);
+
+    imgHotel.setAttribute("src", hotelImage);
+    hotelNameH2.innerHTML = hotelName;
+    hotelStars.innerHTML = hotelRating;
+    reviewsCount.innerHTML = hotelReviewCount;
+    hotelPriceH3.innerHTML = hotelPrice;
+
+    return divHotelCard;
  }
+
+
