04634ea910cd8d993305396060dcfac97ce7f9ab
Alberto Cangialosi <billyballo.res@gmail.com>
2022-09-21 23:26:03 +0200

    first functions (yet to validate) and created PROJECTS.md

---
+++ improve-your-front-end-skills/PROJECTS.md
@@ -0,0 +1,14 @@
+# Improve Your Front End Skills
+
+All 15 projects presented from this [Dev To article][1]
+
+Those are the first 15 of [25 projects][2]
+
+[TOC]
+
+## Project 1
+
+Hex Colors and `linear-gradient()` with a sprinkle of `parseInt(hexStr, base16)` and `number.toString(base16)` (source of these simple convertion <https://www.w3docs.com/snippets/javascript/how-to-convert-decimal-to-hexadecimal-in-javascript.html>)
+
+[1]: https://dev.to/codefoxx/15-beginner-javascript-projects-to-improve-your-front-end-skills-5bcj
+[2]: https://dev.to/codefoxx/25-beginner-javascript-project-ideas-3m9h
--- improve-your-front-end-skills/project1.js
+++ improve-your-front-end-skills/project1.js
@@ -1,4 +1,32 @@
-import { append2body, append2parent, nestingFromJson, HtmlElem } from "../myDomModule.js";
+function numToHex (num) {
+    /* Convert decimal number to hexadecimal string */
+    // checkin input is an integer
+    if (parseInt(num) === num) {
+        return num.toString(16)   // convert with base 16 into hexadecimal
+    } else {
+        throw "TypeError: to get hexadecimal string insert a integer number"
+    };
+};
+
+function hexToNum (hex) {
+    /* Convert hexadecimal string to decimal number */
+    // checkin input is a string
+    if (typeof(hex) === "string") {
+        return parseInt(hex, 16)   // convert with base 16 into decimal
+    } else {
+        throw "TypeError: to get integer number insert a hexadecimal string"
+    };
+};
+
+function hexGen() {
+    /* Hex number Generator */
+    let num = () => {
+        return Math.floor(Math.random() * 255 + 1)
+    };
+    return numToHex(num);
+};
+
+// TODO : validate the functions above, one by one, with all cases
 
 let h1 = document.getElementsByTagName("h1")[0];
 let h2 = document.getElementsByTagName("h2")[0];
@@ -8,5 +36,10 @@ alert(button);
 button.addEventListener("click", function () {
         button.setAttribute("title", "Life is better with C*O*L*O*R*S");
         alert("click event listened");
+
+        color1 = `#${hexGen()}${hexGen()}${hexGen()}`;
+        console.log("color1 = "+ color1);
+        color2 = `#${hexGen()}${hexGen()}${hexGen()}`;
+        console.log("color2 = "+ color2);
     }
 );
