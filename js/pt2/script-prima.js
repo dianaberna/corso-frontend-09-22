dc2c9a0512e9227c4422b9ffb4649df280ecfaf1
dianaberna <bernabei.d@gmail.com>
2022-09-26 11:03:54 +0200

    eventlistener aggiornato

--- js/pt2/script-prima.js
+++ js/pt2/script-prima.js
@@ -1,7 +1,15 @@
-window.addEventListener("load", function () {
-    alert("sono nell'event listener e il body è stato caricato");
+window.addEventListener("load", function (eventoLoad) {
+    /* alert("sono nell'event listener e il body è stato caricato"); */
     console.log("sono nel dom1 nell'event listener");
     document.getElementById("primodiv").style.backgroundColor = "red";
+
+    let bottone = document.getElementById("buttonCiao");
+    bottone.addEventListener("click", function(evento){
+         console.log("bottone cliccato")
+         console.log("stampo il contenuto dell'evento ")
+         console.log(evento.target.id)
+    })
+
 });
 
 alert("ho caricato il file dom.js e il dom non è ancora caricato");
